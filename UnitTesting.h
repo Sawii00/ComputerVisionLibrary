8a7c1c25fbc5796f46cf2b5a3f33d2060a25e7d2
Sawii00 <s.nasturzio@libero.it>
2020-03-20 18:46:54 +0100

    Added UnitTesting

---
+++ UnitTesting.h
@@ -0,0 +1,72 @@
+#pragma once
+#include "image.h"
+#include "filter.h"
+
+void testNormalConvolution1D(Image& image)
+{
+	Filter box_filter(3,3,false);
+	float val = 1.0f/9;
+	float f[9] = {val,val,val,val,val,val,val,val,val};
+	box_filter.build(f, 9);
+	
+	TimedBlock t("Convolution1D");
+	image.convolve1D(box_filter);
+	t.stopTimedBlock();
+	image.displayImage();
+	
+}
+
+void testNormalConvolution3D(Image& image)
+{
+	Filter color_booster(3,3,true);
+	KernelPixel val(0.005f, 0.4f, 0.005f);
+	KernelPixel vals[9] = {val,val,val,val,val,val,val,val,val};
+	color_booster.build(vals, 9);
+	
+	TimedBlock t("Convolution3D");
+	image.convolve3D(color_booster);
+	t.stopTimedBlock();
+	image.displayImage();
+	
+}
+
+void testNormalSeparableConvolution(Image& image)
+{
+	
+	float f_v[3] = { 1,2,1 };
+	float f_h[3] = { -1,0,1 };
+	SeparableFilter vertical(f_v, 3);
+	SeparableFilter horizontal(f_h, 3);
+	
+	
+	TimedBlock t("NormalSeparableConvolution");
+	image.separableConvolution1D(vertical, horizontal);
+	t.stopTimedBlock();
+	
+	image.displayImage();
+	
+	
+}
+
+void testMultiThreadedConvolution(Image& image)
+{
+	
+	float f_v[3] = { 1,2,1 };
+	float f_h[3] = { -1,0,1 };
+	SeparableFilter vertical(f_v, 3);
+	SeparableFilter horizontal(f_h, 3);
+	
+	
+	TimedBlock t("NormalSeparableConvolution");
+	image.convolveThreading(vertical, horizontal);
+	t.stopTimedBlock();
+	
+	image.displayImage();
+	
+	
+}
+
+void testGaussianBlur(Image& image)
+{
+	
+}
--- main.cpp
+++ main.cpp
@@ -4,85 +4,26 @@
 #include <iostream>
 #include <random>
 #include <immintrin.h>
+#include "UnitTesting.h"
+
 
 //TODO(SAWII): read source code of OpenCV... look at TBB, read pdfs, implement image operations simd
 
 int main() {
 	Image img;
-	Image img2;
-	Image img3;
     
 	if (img.loadBMP("img.bmp"))
 	{
-		/*
-		img2 = img;
-		img3 = img;
-        
-		Filter filter(3, 3, false);
-		float f[9] = { -1,-1,-1,-1,8,-1,-1,-1,-1 };
-		filter.build(f, 9);
-        
-		Filter f2(3, 3, true);
-		KernelPixel val(0.005, 0.4, 0.005);
-		KernelPixel vals[9] = { val, val, val, val, val, val ,val ,val, val };
-		f2.build(vals, 9);
-        
-		TimedBlock test("Test");
-		img.convolve1D(filter);
-        
-		img.convolve3D(f2);
-        
-		img += (img2 * 0.8);
-		test.stopTimedBlock();
-        
-		img.displayImage();
-        
-										Filter f1(1, 3, false);
-										Filter f2(3, 1, false);
-										float f_1[3] = { 1,2,1 };
-										float f_2[3] = { -1,0,1 };
-										f1.build(f_1, 3);
-										f2.build(f_2, 3);
-
-										Filter f3(3, 3, true);
-										KernelPixel val(0.005, 0.4, 0.005);
-										KernelPixel vals[9] = { val, val, val, val, val, val ,val ,val, val };
-										f3.build(vals, 9);
-										Timer t;
-										t.start();
-										img2.separableConvolution1D(f1, f2);
-										img2.convolve3D(f3);
-										img2 += (img * 0.6);
-										t.stop();
-
-										std::cout<<"Time: "<<t.elapsedSeconds();
-
-										img2.displayImage();
-
-											Filter gau(7, 7, false);
-											img3.displayImage();
-											Timer t;
-											t.start();
-											if (gau.buildAsGaussian())
-												img3.convolve1D(gau);
-											t.stop();
-											std::cout<<"Time: "<<t.elapsedMilliseconds();
-											img3.displayImage();
-*/
+		Image img2 = img;
+		Image img3 = img;
+		Image img4 = img;
 		
-		float f_v[3] = { 1,2,1 };
-        float f_h[3] = { -1,0,1 };
-        SeparableFilter vertical(f_v, 3);
-        SeparableFilter horizontal(f_h, 3);
-        
-        
-        TimedBlock test("Threaded Convolution");
-        img.convolveThreading(vertical, horizontal);
-        test.stopTimedBlock();
-        
-        img.displayImage();
-        
-        
+		testNormalConvolution1D(img);
+		testNormalConvolution3D(img2);
+		testNormalSeparableConvolution(img3);
+		testMultiThreadedConvolution(img4);
+		
+        system("pause");
 		return 0;
 	}
 }
